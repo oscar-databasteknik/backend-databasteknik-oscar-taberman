ac70d5be79a58d898fac6916e9b9370718dffd74
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-02-17 14:52:07 +0100

    Added: -heroes Dto. -wwwroot\images Installed: -Microsoft.EntityFrameworkCore -Microsoft.EntityFrameworkCore.SqlServer -Microsoft.EntityFrameworkCore.Tools

---
+++ Backend.Presentation.API/Dtos/HeroDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Presentation.API.Dtos;
+
+public sealed record HeroDto
+(
+    string Title,
+    string Header,
+    string Description,
+    string? ImageUrl
+);
--- Backend.Presentation.API/Program.cs
+++ Backend.Presentation.API/Program.cs
@@ -1,14 +1,29 @@
+using Backend.Presentation.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.MapOpenApi();
-
 app.UseHttpsRedirection();
+app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseStaticFiles();
 
+
+app.MapGet("/api/heroes", (HttpRequest request) =>
+{
+    var baseUrl = $"{request.Scheme}://{request.Host}";
+
+    var heroes = new List<HeroDto>
+    {
+        new("Course Online", "Välkommen till studentlivet", "Utveckla dina kunskaper med oss online.", $"{baseUrl}/images/hero-university.jpg")
+    };
+
+    return Results.Ok(heroes);
+});
 
 app.Run();
-
