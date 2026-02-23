75a5f19f47b1940b7e41a8509ad6d12614ac721d
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-02-23 13:10:58 +0100

    - Initial create Migrations

--- Backend.Infrastructure/Data/myAcademyDbContext.cs
+++ Backend.Infrastructure/Data/myAcademyDbContext.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infrastructure.Persistence;
 
 public sealed class MyAcademyDbContext(DbContextOptions<MyAcademyDbContext> options) : DbContext(options)
 {
+    public DbSet<Course> Courses => Set<Course>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.ToTable("Courses");
+
+            entity.HasKey(e => e.Id)
+                  .HasName("Courses_Id_PK");
+
+            entity.Property(e => e.Title)
+                  .IsRequired()
+                  .HasMaxLength(200);
+
+            entity.Property(x => x.Description)
+                  .IsRequired()
+                  .HasMaxLength(2000);
+                
+
+            entity.Property(x => x.Duration)
+                  .IsRequired();
+        });
     }
 }
---
+++ Backend.Infrastructure/Migrations/20260223120333_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Courses_Id_PK", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
