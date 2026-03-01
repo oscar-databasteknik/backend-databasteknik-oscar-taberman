82c53b10e608ddbbd181c6e8c00ddd8aede26717
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-03-01 09:31:02 +0100

    Created mapping for Course.

---
+++ Backend.Presentation.API/Mappings/CourseMapping.cs
@@ -0,0 +1,34 @@
+﻿using Backend.Domain.Entities;
+using Backend.Presentation.API.Dtos;
+
+namespace Backend.Presentation.API.Mappings;
+
+public static class CourseMapping
+{
+    public static CourseResponse ToCourseResponse(this Course course)
+    {
+        return new CourseResponse(
+            course.Id,
+            course.Title,
+            course.Description,
+            course.Duration
+        );
+    }
+
+    public static Course ToCourse(this CreateCourseRequest request)
+    {
+        return new Course
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Duration = request.Duration
+        };
+    }
+
+    public static void UpdateCourse(this Course course, UpdateCourseRequest request)
+    {
+        course.Title = request.Title;
+        course.Description = request.Description;
+        course.Duration = request.Duration;
+    }
+}
